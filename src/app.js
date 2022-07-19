03f8bef35b2595d09a42c0c407faaec7fec8b01f
sangminlee98 <tkdals0975@naver.com>
2022-07-20 01:42:06 +0900

    url-loader 적용(webpack4)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 import "./app.css";
+import nyancat from "./nyancat.jpeg";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `
+    <img src="${nyancat}" />
+  `;
+});
