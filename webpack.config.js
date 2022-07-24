e402beb7d09b710e8cf2644ec1764d8c0521ff9b
sangminlee98 <tkdals0975@naver.com>
2022-07-24 21:02:25 +0900

    custom plugin 만들기

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MyWebpackPlugin = require("./my-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
@@ -25,4 +26,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new MyWebpackPlugin()],
 };
