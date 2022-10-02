2515a1ceff7555be2663cba448131b8dbd70a0bb
sangminlee98 <tkdals0975@naver.com>
2022-10-02 21:40:31 +0900

    hot module replacement

--- src/app.js
+++ src/app.js
@@ -1,18 +1,45 @@
 import "./app.css";
-import nyancat from "./nyancat.jpeg";
-import axios from "axios";
+// import nyancat from "./nyancat.jpeg";
+// import axios from "axios";
+import form from "./form";
+import result from "./result";
+
+let resultEl;
+let formEl;
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const res = await axios.get("/api/users");
-  document.body.innerHTML = `
-    <img src="${nyancat}" />
-    <div class='user_container'></div>
-  `;
-  const container = document.querySelector(".user_container");
-  container.style.color = "white";
-  container.innerHTML = (res.data || [])
-    .map((user) => {
-      return `<div>${user.id}: ${user.name}</div>`;
-    })
-    .join("");
+  // const res = await axios.get("/api/users");
+  // document.body.innerHTML = `
+  //   <img src="${nyancat}" />
+  //   <div class='user_container'></div>
+  // `;
+  // const container = document.querySelector(".user_container");
+  // container.style.color = "white";
+  // container.innerHTML = (res.data || [])
+  //   .map((user) => {
+  //     return `<div>${user.id}: ${user.name}</div>`;
+  //   })
+  //   .join("");
+
+  formEl = document.createElement("div");
+  formEl.innerHTML = form.render();
+  document.body.appendChild(formEl);
+
+  resultEl = document.createElement("div");
+  resultEl.innerHTML = await result.render();
+  document.body.appendChild(resultEl);
 });
+
+if (module.hot) {
+  console.log("핫 모듈 켜짐");
+
+  module.hot.accept("./result", async () => {
+    console.log("result 모듈 변경됨");
+    resultEl.innerHTML = await result.render();
+  });
+
+  module.hot.accept("./form", async () => {
+    console.log("form 모듈 변경됨");
+    formEl.innerHTML = form.render();
+  });
+}
